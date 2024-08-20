eaa1cc57958ea913605009dfee07aa4d8e5d541a
Moritz Frisch <mo_68@gmx.de>
2024-08-20 19:48:40 +0200

    initial release
    Signed-off-by: Moritz Frisch <mo_68@gmx.de>

---
+++ out/build/clang/CMakeFiles/wc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wc.dir/lex.yy.cpp.o"
+  "CMakeFiles/wc.dir/lex.yy.cpp.o.d"
+  "wc"
+  "wc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/clang/CMakeFiles/wc.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for wc.
+# This may be replaced when dependencies are built.
